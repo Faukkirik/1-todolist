d8a147b6eac75d3f02f90bc2e266cbbe61f7c4dc
Faukkirik <kote_33419@mai.ru>
2022-12-10 08:52:14 +0300

    Commit 1

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+const tasks_1: Array<TaskType> = [
+    {id: 1, title: "HTML & CSS", isDone: true},
+    {id: 2, title: "ES6 & JS", isDone: true},
+    {id: 3, title: "React", isDone: false},
+]
+
+const tasks_2: Array<TaskType> = [
+    {id: 1, title: "HTML & CSS", isDone: true},
+    {id: 2, title: "ES6 & JS", isDone: true},
+    {id: 3, title: "React", isDone: false},
+]
+function App() {
+    const todoListTitle_1: string = "What to learn"
+    const todoListTitle_2: string = "What to buy"
+
+    return (
+        <div className="App">
+            <TodoList title={todoListTitle_1} tasks={tasks_1}/>
+            <TodoList title={todoListTitle_2} tasks={tasks_2}/>
+        </div>
+    );
+}
+
+export default App;
