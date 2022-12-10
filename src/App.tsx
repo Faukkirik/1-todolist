c1144bd64173f5368dbed0d2b519b7233f00130f
Faukkirik <kote_33419@mai.ru>
2022-12-10 12:14:46 +0300

    Commit 2 (tasks.map)

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ const tasks_1: Array<TaskType> = [
     {id: 1, title: "HTML & CSS", isDone: true},
     {id: 2, title: "ES6 & JS", isDone: true},
     {id: 3, title: "React", isDone: false},
+    {id: 4, title: "React", isDone: false},
 ]
 
 const tasks_2: Array<TaskType> = [
-    {id: 1, title: "HTML & CSS", isDone: true},
-    {id: 2, title: "ES6 & JS", isDone: true},
-    {id: 3, title: "React", isDone: false},
+    {id: 1, title: "pencil", isDone: false},
+    {id: 2, title: "pen", isDone: true},
+    {id: 3, title: "blocknote", isDone: true},
 ]
 function App() {
     const todoListTitle_1: string = "What to learn"
